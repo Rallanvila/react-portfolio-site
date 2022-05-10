e69f86c5049c33775dcf932c331fc04d641ec6d7
Allan Vila <r.allanvila@gmail.com>
2022-05-09 21:25:34 -0400

    added favicon

--- components/HeadTag/HeadTag.jsx
+++ components/HeadTag/HeadTag.jsx
@@ -4,10 +4,10 @@ import React from "react";
 const HeadTag = () => {
 	return (
 		<Head>
-			<title>Allan Vila | Full Stack Developer</title>
+			<title>Allan Vila | Front-End Engineer</title>
 			<meta
 				name="description"
-				content="I'm a full-stack web developer for Naval Systems Inc. We focus on high-end clearance projects for the Department of Defense and various military branches. I'm the front-end engineer and design expert for the team. I'm tasked with beautifying our existing projects, making them have a more modern look and feel while also adding and improving existing functionality on the backend."
+				content="I'm a full-stack web developer with a front-end specialty for Naval Systems Inc. We focus on high-end clearance projects for the Department of Defense and various military branches. I'm the front-end engineer and design expert for the team. I'm tasked with beautifying our existing projects, making them have a more modern look and feel while also adding and improving existing functionality on the backend."
 			/>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta property="og:title" content="Allan Vila | Full-Stack Developer" />
@@ -15,8 +15,17 @@ const HeadTag = () => {
 			<meta property="og:url" content="https://www.rallanvila.com" />
 			<meta property="og:image" content="/img/MyCard.png" />
 			<meta name="twitter:site" content="@RAllanVila" />
-			<link rel="apple-touch-icon" sizes="120x120" href="/img/MyLogoIcon.svg" />
-			<link
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+
+		</Head>
+	);
+};
+
+export default HeadTag;
+			 <link
 				rel="icon"
 				type="image/png"
 				sizes="32x32"
@@ -27,9 +36,5 @@ const HeadTag = () => {
 				type="image/png"
 				sizes="16x16"
 				href="/img/MyLogoIcon.svg"
-			/>
-		</Head>
-	);
-};
-
-export default HeadTag;
+			/> 
+			<link rel="apple-touch-icon" sizes="120x120" href="/img/MyLogoIcon.svg" /> 
