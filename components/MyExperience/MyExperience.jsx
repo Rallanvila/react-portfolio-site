6651fabfe13a0e8f138647d7161a5532f240369d
Allan Vila <r.allanvila@gmail.com>
2023-05-21 18:53:50 -0400

    feat: timeline,  job component added

--- components/MyExperience/MyExperience.jsx
+++ components/MyExperience/MyExperience.jsx
@@ -1,73 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Experience from '../../public/img/experience.svg';
-import nsilogo from '../../public/img/nsi-logo.png';
-import avbLogo from '../../public/img/avb-logo.png';
 import Image from 'next/image';
-
-const nsiExperience = {
-	image: nsilogo,
-	alt: 'nsi logo',
-	whatWeDo:
-		'Naval Systems supports the Department of Defense. I work on secret-level clearance projects that help with our ongoing national-security well-being. My work is currently active with support NavAir, US Air Force, Missile Defense Agency, DASA DE&C, and more.',
-	whatIDo:
-		"I'm the Front-End Engineer and design expert for the team. I build, maintain and improve various dashboards on several projects that track and update NavAir and the Air Force with crucial data regarding their defensive operations.",
-	whatIdo2:
-		'I also help design and develop client rebranding efforts. Attendmeetings with stakeholders to understand the mission, vision, and keyperformance indicators the client is trying to improve to create aproduct that will lead to higher conversion levels enabling the salesteams.',
-};
-const avbExperience = {
-	image: avbLogo,
-	alt: 'nsi logo',
-	whatWeDo:
-		'Naval Systems supports the Department of Defense. I work on secret-level clearance projects that help with our ongoing national-security well-being. My work is currently active with support NavAir, US Air Force, Missile Defense Agency, DASA DE&C, and more.',
-	whatIDo:
-		"I'm the Front-End Engineer and design expert for the team. I build, maintain and improve various dashboards on several projects that track and update NavAir and the Air Force with crucial data regarding their defensive operations.",
-	whatIdo2:
-		'I also help design and develop client rebranding efforts. Attendmeetings with stakeholders to understand the mission, vision, and keyperformance indicators the client is trying to improve to create aproduct that will lead to higher conversion levels enabling the salesteams.',
-};
+import { avbExperience, nsiExperience } from '../../data/jobs';
+import { MyTimeline } from './Timeline';
 
 export default function MyExperience() {
+	const [hidden, setHidden] = useState(false);
+
+	const handleHidden = (hidden) => {
+		setHidden(!hidden);
+	};
+
 	return (
-		<section
-			id='experience'
-			className='max-w-6xl mt-8 mb-8 px-8 mx-auto md:flex md:mt-32'>
-			<div
-				className='flex flex-col-reverse md:flex-col md:w-1/2 '
-				data-aos='fade-right'>
-				<h2 className='text-xl font-bold mb-12'>My Experience:</h2>
-				<div className='mb-8 flex justify-center md:justify-start md:mb-0'>
-					<Image
-						src={Experience}
-						width={452}
-						height={425}
-						alt='guy sitting on computer'
-					/>
+		<>
+			<section
+				id='experience'
+				className='max-w-6xl mt-8 mb-8 px-8 mx-auto md:flex md:mt-32 relative'>
+				<div
+					className='flex flex-col-reverse md:flex-col md:w-1/2 '
+					data-aos='fade-right'>
+					<h2 className='text-xl font-bold mb-12'>My Experience:</h2>
+					<div className='mb-8 flex justify-center md:justify-start md:mb-0'>
+						<Image
+							src={Experience}
+							width={452}
+							height={425}
+							alt='guy sitting on computer'
+						/>
+					</div>
 				</div>
-			</div>
-			<article className='md:w-1/2 md:ml-12' data-aos='fade-left'>
-				<div className='max-w-xs mb-8'>
-					<Image src={nsilogo} width={371} height={79} alt='nsi logo' />
-				</div>
-				<h3 className='text-lg font-semibold mb-2'>What we do:</h3>
-				<p className='mb-8'>
-					Naval Systems supports the Department of Defense. I work on secret-level
-					clearance projects that help with our ongoing national-security well-being.
-					My work is currently active with support NavAir, US Air Force, Missile
-					Defense Agency, DASA DE&C, and more.
-				</p>
-				<h3 className='text-lg font-semibold mb-2'>What I do:</h3>
-				<p className='mb-5'>
-					I&apos;m the Front-End Engineer and design expert for the team. I build,
-					maintain and improve various dashboards on several projects that track and
-					update NavAir and the Air Force with crucial data regarding their defensive
-					operations.
-				</p>
-				<p>
-					I also help design and develop client rebranding efforts. Attend meetings
-					with stakeholders to understand the mission, vision, and key performance
-					indicators the client is trying to improve to create a product that will
-					lead to higher conversion levels enabling the sales teams.
-				</p>
-			</article>
-		</section>
+				<Job hidden={hidden} jobName={avbExperience} />
+				<Job hidden={!hidden} jobName={nsiExperience} />
+				<MyTimeline className='absolute top-1/2 right-0 transform -translate-y-1/2' />
+			</section>
+		</>
 	);
 }
+
+const Job = (props) => {
+	const { hidden, jobName } = props;
+	const { image, alt, whatWeDo, whatIDo, whatIDo2, duration } = jobName;
+
+	if (hidden) return null;
+
+	return (
+		<article className='md:w-1/2 md:ml-12' data-aos='fade-left'>
+			<div className='max-w-xs mb-8'>
+				<Image src={image} width={371} height={79} alt={alt} />
+			</div>
+
+			<h3 className='text-lg font-semibold mb-2'>What we do:</h3>
+			<p className='mb-8'>{whatWeDo}</p>
+			<h3 className='text-lg font-semibold mb-2'>What I do:</h3>
+			<p className='mb-5'>{whatIDo}</p>
+			<p>{whatIDo2}</p>
+		</article>
+	);
+};
---
+++ data/jobs.js
@@ -0,0 +1,27 @@
+import nsiLogo from '../public/img/nsi-logo.png';
+import avbLogo from '../public/img/avb-logo.png';
+
+export const nsiExperience = {
+	jobName: 'Naval Systems Inc.',
+	image: nsiLogo,
+	alt: 'nsi logo',
+	whatWeDo:
+		'Naval Systems supports the Department of Defense. I work on secret-level clearance projects that help with our ongoing national-security well-being. My work is currently active with support NavAir, US Air Force, Missile Defense Agency, DASA DE&C, and more.',
+	whatIDo:
+		"I'm the Front-End Engineer and design expert for the team. I build, maintain and improve various dashboards on several projects that track and update NavAir and the Air Force with crucial data regarding their defensive operations.",
+	whatIDo2:
+		'I also help design and develop client rebranding efforts. Attendmeetings with stakeholders to understand the mission, vision, and keyperformance indicators the client is trying to improve to create aproduct that will lead to higher conversion levels enabling the salesteams.',
+	duration: '2021 - 2022',
+};
+export const avbExperience = {
+	jobName: 'AVB Marketing',
+	image: avbLogo,
+	alt: 'nsi logo',
+	whatWeDo:
+		'Naval Systems supports the Department of Defense. I work on secret-level clearance projects that help with our ongoing national-security well-being. My work is currently active with support NavAir, US Air Force, Missile Defense Agency, DASA DE&C, and more.',
+	whatIDo:
+		"I'm the Front-End Engineer and design expert for the team. I build, maintain and improve various dashboards on several projects that track and update NavAir and the Air Force with crucial data regarding their defensive operations.",
+	whatIDo2:
+		'I also help design and develop client rebranding efforts. Attendmeetings with stakeholders to understand the mission, vision, and keyperformance indicators the client is trying to improve to create aproduct that will lead to higher conversion levels enabling the salesteams.',
+	duration: '2022 - Present',
+};
