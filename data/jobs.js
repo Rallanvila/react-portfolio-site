0c754c74b90cb9790f357044b853294acc099ca1
Allan Vila <r.allanvila@gmail.com>
2024-06-19 17:18:55 -0400

    refactor(job-data): made it an array so I could map

--- data/jobs.js
+++ data/jobs.js
@@ -1,27 +1,33 @@
 import nsiLogo from '../public/img/nsi-logo.png';
 import avbLogo from '../public/img/avb-logo.png';
 
-export const nsiExperience = {
-	jobName: 'Naval Systems Inc.',
-	image: nsiLogo,
-	alt: 'nsi logo',
-	whatWeDo:
-		'Naval Systems supports the Department of Defense. I work on secret-level clearance projects that help with our ongoing national-security well-being. My work is currently active with support NavAir, US Air Force, Missile Defense Agency, DASA DE&C, and more.',
-	whatIDo:
-		"I'm the Front-End Engineer and design expert for the team. I build, maintain and improve various dashboards on several projects that track and update NavAir and the Air Force with crucial data regarding their defensive operations.",
-	whatIDo2:
-		'I also help design and develop client rebranding efforts. Attendmeetings with stakeholders to understand the mission, vision, and keyperformance indicators the client is trying to improve to create aproduct that will lead to higher conversion levels enabling the salesteams.',
-	duration: '2021 - 2022',
-};
-export const avbExperience = {
-	jobName: 'AVB Marketing',
-	image: avbLogo,
-	alt: 'avb marketing logo',
-	whatWeDo:
-		'AVB Marketing is a company that specializes in marketing services. They provide strategic planning, digital marketing, advertising campaigns, and branding solutions to help businesses reach their target audience and achieve their marketing goals.',
-	whatIDo:
-		"In my role as a Software Engineer at AVB, I have the exciting opportunity to bring an entirely new user interface to life for our established software system. Our in-house software was built by our talented .net development team, and now we're taking it to the next level by creating a separate application using React with NextJS. This new interface needs to be fast, responsive, efficient, and future-friendly. ",
-	whatIDo2:
-		"Working closely with our front-end and.net teams, my responsibilities include ensuring the technical feasibility of UI/UX designs, building reusable code, and libraries, and collaborating with other development teams. I bring expertise in modern front-end development, strong Typescript skills, and experience with RESTful Web Services. Additionally, I work with Redux Toolkit, Jira, code-review pull-requests, and handle releases. I'm passionate about creating intuitive and visually appealing interfaces that enhance user experiences, and coaching/developing my team.",
-	duration: '2022 - Present',
-};
+const experience = [
+  {
+    jobName: 'AVB Marketing',
+    time: '2022 - Present',
+    image: avbLogo,
+    alt: 'avb marketing logo',
+    whatWeDo:
+      'AVB Marketing is a company that specializes in marketing services. They provide strategic planning, digital marketing, advertising campaigns, and branding solutions to help businesses reach their target audience and achieve their marketing goals.',
+    whatIDo:
+      "In my role as a Software Engineer at AVB, I have the exciting opportunity to bring an entirely new user interface to life for our established software system. Our in-house software was built by our talented .net development team, and now we're taking it to the next level by creating a separate application using React with NextJS. This new interface needs to be fast, responsive, efficient, and future-friendly. ",
+    whatIDo2:
+      "Working closely with our front-end and.net teams, my responsibilities include ensuring the technical feasibility of UI/UX designs, building reusable code, and libraries, and collaborating with other development teams. I bring expertise in modern front-end development, strong Typescript skills, and experience with RESTful Web Services. Additionally, I work with Redux Toolkit, Jira, code-review pull-requests, and handle releases. I'm passionate about creating intuitive and visually appealing interfaces that enhance user experiences, and coaching/developing my team.",
+    duration: '2022 - Present',
+  },
+  {
+    jobName: 'Naval Systems Inc.',
+    time: '2021 - 2022',
+    image: nsiLogo,
+    alt: 'nsi logo',
+    whatWeDo:
+      'Naval Systems supports the Department of Defense. I work on secret-level clearance projects that help with our ongoing national-security well-being. My work is currently active with support NavAir, US Air Force, Missile Defense Agency, DASA DE&C, and more.',
+    whatIDo:
+      "I'm the Front-End Engineer and design expert for the team. I build, maintain and improve various dashboards on several projects that track and update NavAir and the Air Force with crucial data regarding their defensive operations.",
+    whatIDo2:
+      'I also help design and develop client rebranding efforts. Attendmeetings with stakeholders to understand the mission, vision, and keyperformance indicators the client is trying to improve to create aproduct that will lead to higher conversion levels enabling the salesteams.',
+    duration: '2021 - 2022',
+  },
+];
+
+export default experience;
