ba3ee1ca3e6c69b42bb48d8bdc9455e17f5c47de
Allan Vila <r.allanvila@gmail.com>
2024-06-19 17:35:33 -0400

    refactor(head): update HeadTag content

--- components/HeadTag/HeadTag.jsx
+++ components/HeadTag/HeadTag.jsx
@@ -1,60 +1,47 @@
-import Head from "next/head";
-import React from "react";
+import Head from 'next/head';
+import React from 'react';
 
 const HeadTag = () => {
-	return (
-		<Head>
-			<title>Allan Vila | Front-End Engineer</title>
-			<meta
-				name="description"
-				content="I'm a full-stack web developer with a front-end specialty for Naval Systems Inc. We focus on high-end clearance projects for the Department of Defense and various military branches. I'm the front-end engineer and design expert for the team. I'm tasked with beautifying our existing projects, making them have a more modern look and feel while also adding and improving existing functionality on the backend."
-			/>
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<meta property="og:title" content="Allan Vila | Full-Stack Developer" />
-			<meta property="og:type" content="website" />
-			<meta property="og:url" content="https://www.rallanvila.com" />
-			<meta property="og:image" content="/img/MyCard.png" />
-			<meta name="twitter:site" content="@RAllanVila" />
-			<meta name="twitter:title" content="Allan Vila | Front-End Engineer" />
-			<meta
-				name="twitter:description"
-				content="I'm a full-stack engineer with a front-end framework and design specialty"
-			/>
-			<meta name="twitter:image" content="/img/AllOneLogo.png" />
-			<meta name="twitter:card" content="/img/MyCard.png" />
-			<link
-				rel="apple-touch-icon"
-				sizes="180x180"
-				href="/apple-touch-icon.png"
-			/>
-			<link
-				rel="icon"
-				type="image/png"
-				sizes="32x32"
-				href="/favicon-32x32.png"
-			/>
-			<link
-				rel="icon"
-				type="image/png"
-				sizes="16x16"
-				href="/favicon-16x16.png"
-			/>
-			<link rel="manifest" href="/site.webmanifest" />
-		</Head>
-	);
+  return (
+    <Head>
+      <title>Allan Vila | Software Engineer</title>
+      <meta
+        name='description'
+        content="I'm Software Engineer at AVB Marketing that creates and maintains dynamic web interfaces using React.js. I collaborate with designers and backend developers, optimize performance, and perform testing and debugging. I stay updated with industry trends, apply best practices, and participate in code reviews and team meetings. This role ensures high-quality, user-friendly web applications that support the company’s digital marketing efforts."
+      />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta property='og:title' content='Allan Vila | Software Engineer' />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://www.rallanvila.com' />
+      <meta property='og:image' content='/img/MyCard.png' />
+      <meta name='twitter:site' content='@RAllanVila' />
+      <meta name='twitter:title' content='Allan Vila | Software Engineer' />
+      <meta
+        name='twitter:description'
+        content="I'm a Software Engineer with a front-end framework and design specialty"
+      />
+      <meta name='twitter:image' content='/img/AllOneLogo.png' />
+      <meta name='twitter:card' content='/img/MyCard.png' />
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon-16x16.png'
+      />
+      <link rel='manifest' href='/site.webmanifest' />
+    </Head>
+  );
 };
 
 export default HeadTag;
-//  <link
-// 	rel="icon"
-// 	type="image/png"
-// 	sizes="32x32"
-// 	href="/img/MyLogoIcon.svg"
-// />
-// <link
-// 	rel="icon"
-// 	type="image/png"
-// 	sizes="16x16"
-// 	href="/img/MyLogoIcon.svg"
-// />
-// <link rel="apple-touch-icon" sizes="120x120" href="/img/MyLogoIcon.svg" />
