8e330458ebd3492a1d6b83ea1718122d6dbc65bf
Allan Vila <r.allanvila@gmail.com>
2024-07-02 21:44:47 -0400

    refactor: updated About Me

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -3,41 +3,42 @@ import Image from "next/image";
 import AboutPic from "../../public/img/about-pic.svg";
 
 export default function About() {
-	return (
-		<section className="diagonal-box">
-			<div className="content px-8 mx-auto flex flex-col-reverse justify-between max-w-6xl md:flex-row">
-				<article data-aos="fade-in">
-					<h2 className="text-xl mb-2 font-semibold">About Me:</h2>
-					<p className="md:max-w-lg sm:max-w-full">
-						I am a successful 10+ years business sales professional turned
-						developer after covid. Once covid hit, I spent more time at home
-						with my family and noticed that we needed a change. This is when I
-						found my passion for software development.
-						<br />
-						<br />
-						While managing a 50-60hr workweek, school pick-ups, drop-offs,
-						dinner, and homework from 9:30 pm to 2:30 am, I&apos;d code to then
-						wake up at 7 am to rinse and repeat daily. I was hired as a
-						full-time developer in 7 months.
-						<br />
-						<br />
-						When I&apos;m not geeking out in front of my screen, I&apos;m a bit
-						of an adrenaline junky. I love watching movies, enjoy spending time
-						with the love of my life Kim, and two children, and finding new
-						memorable experiences for the family.
-					</p>
-				</article>
-				<div
-					data-aos="fade-in"
-					className=" sm:flex sm:items-center sm:mx-auto sm:mb-8 md:mr-0 md:ml-8">
-					<Image
-						src={AboutPic}
-						width={408}
-						height={303}
-						alt="guy sitting in mountain"
-					/>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="diagonal-box">
+      <div className="content px-8 mx-auto flex flex-col-reverse justify-between max-w-6xl md:flex-row">
+        <article data-aos="fade-in">
+          <h2 className="text-xl mb-2 font-semibold">About Me:</h2>
+          <p className="md:max-w-lg sm:max-w-full">
+            I am a dedicated software engineer with over 10 years of experience
+            in business sales, seamlessly transitioning into development during
+            the Covid-19 pandemic. My journey into software development was
+            fueled by a desire for change and a newfound passion for coding.
+            <br />
+            <br />
+            Balancing a demanding 50–60 hour workweek with family
+            responsibilities, I dedicated late-night hours to mastering coding,
+            eventually becoming a full-time developer in just seven months. My
+            transition showcases my adaptability and commitment to personal
+            growth.
+            <br />
+            <br />
+            Outside of work, I am an adventure enthusiast who enjoys movies and
+            cherishes quality time with my wife Kim and our two children,
+            constantly seeking new experiences to create lasting memories.
+          </p>
+        </article>
+        <div
+          data-aos="fade-in"
+          className=" sm:flex sm:items-center sm:mx-auto sm:mb-8 md:mr-0 md:ml-8"
+        >
+          <Image
+            src={AboutPic}
+            width={408}
+            height={303}
+            alt="guy sitting in mountain"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
